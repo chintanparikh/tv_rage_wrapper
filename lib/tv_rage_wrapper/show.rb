688ea2f1a60e9f1cffbd4c549fe68b17fee6e668
chintanparikh <parikhcv@gmail.com>
2013-04-01 19:39:18 -0400

    Fixed whitespace issues in show.rb

--- lib/tv_rage_wrapper/show.rb
+++ lib/tv_rage_wrapper/show.rb
@@ -1,57 +1,57 @@
 require 'httparty'
 
 module TvRageWrapper
-	class Show
-		include HTTParty
+  class Show
+    include HTTParty
 
-		SEARCH_PATH = '/search.php?show='
-		EPISODES_PATH = '/episode_list.php?sid='
+    SEARCH_PATH = '/search.php?show='
+    EPISODES_PATH = '/episode_list.php?sid='
  
-		attr_reader :name, :full_name, :episodes, :genres, :id, :country, :status, :classification, :started, :ended, :seasons
- 		
- 		def initialize(name)
- 			@name = name
- 			@full_name = ''
-                  @episodes = Hash.new{ [] }
-                  @genres = []
-                  @id = ''
-                  @country = ''
-                  @status = ''
-                  @classification = ''
-                  @started = 0
-                  @ended = 0
-                  @seasons = 0
+    attr_reader :name, :full_name, :episodes, :genres, :id, :country, :status, :classification, :started, :ended, :seasons
+    
+    def initialize(name)
+      @name = name
+      @full_name = ''
+      @episodes = Hash.new{ [] }
+      @genres = []
+      @id = ''
+      @country = ''
+      @status = ''
+      @classification = ''
+      @started = 0
+      @ended = 0
+      @seasons = 0
 
-                  # Make the get request to search for the show
-                  response = self.class.get(Api::base_uri + SEARCH_PATH + name)
-                  data = response.parsed_response['Results']['show'][0]
+      # Make the get request to search for the show
+      response = self.class.get(Api::base_uri + SEARCH_PATH + name)
+      data = response.parsed_response['Results']['show'][0]
 
-                  # Set instance variables
-                  @full_name = data['name']
-                  @genres = data['genres']['genre']
-                  @id = data['showid']
-                  @country = data['country']
-                  @status = data['status']
-                  @classification = data['classification']
-                  @started = data['started']
-                  @ended = data['ended']
-                  @seasons = data['seasons']
+      # Set instance variables
+      @full_name = data['name']
+      @genres = data['genres']['genre']
+      @id = data['showid']
+      @country = data['country']
+      @status = data['status']
+      @classification = data['classification']
+      @started = data['started']
+      @ended = data['ended']
+      @seasons = data['seasons']
 
-                  ## Generate list of episodes and episode objects
-                  response = self.class.get(Api::base_uri + EPISODES_PATH + @id)
-                  data = response.parsed_response['Show']['Episodelist']['Season']
-            			
-            			episodes = Hash.new{ [] }
-            			data.each { |season|
-            			  episodes[season["no"].to_i] = season["episode"].inject({}) {|hash, elem| 
-            			  	hash.merge!(elem["seasonnum"].to_i => elem);
-            			  }.each do |hash, e| 
-            			  		if @episodes[season["no"].to_i].empty? 
-            			  			@episodes[season["no"].to_i] = Hash.new
-            			  		end
-            			  		@episodes[season["no"].to_i][e["seasonnum"].to_i] = Episode.new e["epnum"].to_i, e["seasonnum"].to_i, e["prodnum"].to_i, e["airdate"], e["link"], e["title"]
-            				end
-            			}
-            		end
-            	end
+      ## Generate list of episodes and episode objects
+      response = self.class.get(Api::base_uri + EPISODES_PATH + @id)
+      data = response.parsed_response['Show']['Episodelist']['Season']
+      
+      episodes = Hash.new{ [] }
+      data.each { |season|
+        episodes[season["no"].to_i] = season["episode"].inject({}) {|hash, elem| 
+          hash.merge!(elem["seasonnum"].to_i => elem);
+        }.each do |hash, e| 
+            if @episodes[season["no"].to_i].empty? 
+              @episodes[season["no"].to_i] = Hash.new
+            end
+            @episodes[season["no"].to_i][e["seasonnum"].to_i] = Episode.new e["epnum"].to_i, e["seasonnum"].to_i, e["prodnum"].to_i, e["airdate"], e["link"], e["title"]
+        end
+      }
+    end
+  end
 end
